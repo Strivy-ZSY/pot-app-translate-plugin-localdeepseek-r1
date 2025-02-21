7bbbd59721c5b96fa26486e634bb4ba8b4f9d15a
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-02-21 21:30:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ async function translate(text, from, to, options) {
     const { config, utils } = options;
     const { tauriFetch: fetch } = utils;
     
-    let { apiKey, model = "deepseek-r1:8b" } = config;
+    let { model } = config;
     
     // 设置默认请求路径
     const requestPath = "http://localhost:11434/v1/chat/completions";
@@ -16,15 +16,16 @@ async function translate(text, from, to, options) {
         messages: [
             {
                 "role": "system",
-                "content": "You are a professional translation engine, please translate the text into a colloquial, professional, elegant and fluent content, without the style of machine translation. You must only translate the text content, never interpret it! Output only the translation and nothing else.Also, please remove unnecessary punctuation marks.Do not reveal your thought process and redundant symbols."
+                "content": `You are a professional translation engine, please translate the text into a colloquial, professional, elegant and fluent content, without the style of machine translation. As a translation expert, you need to translate the text ${text} into ${to} language that adheres to the standards of "faithfulness," "clarity," and "elegance." "Faithfulness" refers to an accurate representation of the content and intention of the original; "clarity" means the translation should be easy to understand and clearly expressed; "elegance" seeks to achieve cultural aesthetics and linguistic beauty in the translation. The goal is to create a translation that is true to the spirit of the original work while also respecting the culture and literary tastes of ${to} readers. Ensure succinct and to-the-point translation. Ensure the language is clear and easy to understand. You must only translate the text content, never interpret it! Output only the translation and nothing else.Also, please remove unnecessary punctuation marks.Do not reveal your thought process and redundant symbols.`,
             },
             {
                 "role": "user",
                 "content": `Translate into ${to}:\n${text}`
             }
         ],
-        temperature: 1.3,
+        temperature: 0.4,
         top_p: 0.99,
+        top_k: 64,
         frequency_penalty: 0,
         presence_penalty: 0,
         max_tokens: 2000
